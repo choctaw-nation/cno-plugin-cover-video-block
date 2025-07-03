a964b21b1978c70d8bbae8f4b16614d82ceafa3b
KJ Roelke <kroelke@choctawnation.com>
2025-07-03 16:02:51 -0500

    upgrade to v1.1

--- src/cno-plugin-cover-video-block/index.tsx
+++ src/cno-plugin-cover-video-block/index.tsx
@@ -10,6 +10,7 @@ import {
 	PanelBody,
 	PanelRow,
 	RangeControl,
+	ToggleControl,
 } from '@wordpress/components';
 import { homeButton } from '@wordpress/icons';
 import './style.scss';
@@ -24,6 +25,8 @@ const innerBlocksArgs = {
 			'cno-lite-vimeo/cno-plugin-lite-vimeo-block',
 			{
 				lock: { move: true, remove: true },
+				spacing: { padding: 0 },
+				align: 'full',
 			},
 		],
 		[
@@ -33,12 +36,72 @@ const innerBlocksArgs = {
 				className:
 					'wp-block-cno-cover-video-cno-plugin-cover-video-block__content-container',
 				lock: { move: true, remove: true },
+				align: 'full',
 				layout: {
 					type: 'constrained',
 					orientation: 'vertical',
 				},
 			},
-			[ [ 'core/heading', { placeholder: 'Enter heading...' } ] ],
+			[
+				[
+					'core/heading',
+					{
+						placeholder: 'Enter heading...',
+						style: {
+							spacing: {
+								margin: {
+									top: 0,
+									bottom: 0,
+								},
+							},
+							typography: {
+								textTransform: 'uppercase',
+							},
+						},
+						textAlign: 'center',
+					},
+				],
+				[
+					'core/paragraph',
+					{
+						placeholder: 'Enter subheading...',
+						align: 'center',
+						style: {
+							typography: {
+								textTransform: 'uppercase',
+							},
+							fontSize: 'h6',
+							fontFamily: 'pill-gothic-300mg',
+						},
+					},
+				],
+				[
+					'core/buttons',
+					{
+						layout: {
+							type: 'flex',
+							justifyContent: 'center',
+						},
+					},
+					[
+						[
+							'core/button',
+							{
+								placeholder: 'Enter CTA Text',
+								typography: {
+									letterCase: 'uppercase',
+									fontSize: 'var:preset|font-size|sm',
+								},
+								color: {
+									text: {
+										color: '#fff',
+									},
+								},
+							},
+						],
+					],
+				],
+			],
 		],
 	],
 	renderAppender: false,
@@ -48,7 +111,8 @@ const innerBlocksArgs = {
 registerBlockType( metadata.name, {
 	icon: homeButton,
 	edit: ( { attributes, setAttributes } ) => {
-		const { overlayColor, minHeight } = attributes;
+		const { overlayColor, minHeight, hasDropShadow, contentBgColor } =
+			attributes;
 		const themeColors = select( 'core/block-editor' )?.getSettings()
 			?.colors || [
 			{
@@ -64,10 +128,13 @@ registerBlockType( metadata.name, {
 		return (
 			<>
 				<InspectorControls>
-					<Panel>
-						<PanelBody initialOpen={ true }>
+					<Panel header="Cover (Video) Block Settings">
+						<PanelBody title="Cover Settings" initialOpen={ true }>
 							<PanelRow>
 								<RangeControl
+									__next40pxDefaultSize
+									__nextHasNoMarginBottom
+									__shouldNotWarnDeprecated36pxSize
 									value={ minHeight }
 									onChange={ ( minHeight ) =>
 										setAttributes( { minHeight } )
@@ -75,7 +142,6 @@ registerBlockType( metadata.name, {
 									label="Set a min height for the container on desktop screens"
 									help="Height in pixels"
 									min={ 100 }
-									defaultValue={ 430 }
 									max={ 900 }
 								/>
 							</PanelRow>
@@ -88,7 +154,7 @@ registerBlockType( metadata.name, {
 								</p>
 								<ColorPalette
 									aria-labelledby="overlay-color-label"
-									value={ overlayColor }
+									value={ overlayColor || undefined }
 									colors={ themeColors }
 									enableAlpha={ true }
 									onChange={ ( overlayColor ) =>
@@ -97,6 +163,35 @@ registerBlockType( metadata.name, {
 								/>
 							</div>
 						</PanelBody>
+						<PanelBody title="Content Settings">
+							<div style={ { marginBlock: '1.5rem' } }>
+								<p
+									id="content-bg-color-label"
+									style={ { fontWeight: '700' } }
+								>
+									Select a content background color:
+								</p>
+								<ColorPalette
+									aria-labelledby="content-bg-color-label"
+									value={ contentBgColor || undefined }
+									colors={ themeColors }
+									enableAlpha={ true }
+									onChange={ ( contentBgColor ) =>
+										setAttributes( { contentBgColor } )
+									}
+								/>
+							</div>
+							<PanelRow>
+								<ToggleControl
+									__nextHasNoMarginBottom
+									label="Enable Drop Shadow"
+									checked={ hasDropShadow }
+									onChange={ ( val ) =>
+										setAttributes( { hasDropShadow: val } )
+									}
+								/>
+							</PanelRow>
+						</PanelBody>
 					</Panel>
 				</InspectorControls>
 				<div
@@ -104,7 +199,11 @@ registerBlockType( metadata.name, {
 						useBlockProps( {
 							style: {
 								backgroundColor: overlayColor,
+								'--content-bg-color': contentBgColor,
 								'--custom-minHeight': `${ minHeight }px`,
+								'--drop-shadow': hasDropShadow
+									? '2px 2px 8px rgba(0, 0, 0, 0.5)'
+									: 'none',
 							},
 						} ),
 						innerBlocksArgs
@@ -113,13 +212,19 @@ registerBlockType( metadata.name, {
 			</>
 		);
 	},
-	save: ( { attributes: { overlayColor, minHeight } } ) => (
+	save: ( {
+		attributes: { overlayColor, minHeight, hasDropShadow, contentBgColor },
+	} ) => (
 		<div
 			{ ...useInnerBlocksProps.save(
 				useBlockProps.save( {
 					style: {
 						backgroundColor: overlayColor,
+						'--content-bg-color': contentBgColor,
 						'--custom-minHeight': `${ minHeight }px`,
+						'--drop-shadow': hasDropShadow
+							? '2px 2px 8px rgba(0, 0, 0, 0.5)'
+							: 'none',
 					},
 				} )
 			) }
